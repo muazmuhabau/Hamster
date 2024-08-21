69c43aa1052a082db772d706f9e804722f40e520
muazmuhabau <muazmuhaba222@gmail.com>
2024-08-21 13:51:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
     promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'
         },
          7 : {
-        name :' Polysphere '
+        name :' Polysphere ',
         appToken: "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71",
         promoId: "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71"
   },
